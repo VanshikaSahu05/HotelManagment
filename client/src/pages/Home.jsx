5b694f6402a217c2e1758121c1bbfd5613ead429
vanshika sahu <vanshikasahu281105@gmail.com>
2026-04-16 22:40:58 +0530

    Small Change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import FeatureDestination from '../components/FeatureDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
 import NewsLetter from '../components/NewsLetter'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
